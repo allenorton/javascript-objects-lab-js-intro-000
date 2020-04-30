5abd28aec2ccc73e3296b2440ab773865f7d1e28
Elizabeth Allene Norton <norton.allene@gmail.com>
2020-04-30 20:42:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var car = {make : "nissan", model : "sentra", color : "silver"}
 function updateObjectWithKeyAndValue(object, key, value){
-  Object.assign(car, {key : value})
-  return car
+  Object.assign(object, {key : value})
+  return Object
 }
